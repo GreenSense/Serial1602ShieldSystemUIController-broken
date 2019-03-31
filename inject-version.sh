e5af0f2aa5ce4a5c93bdf3c5ca6bad551d8f2ccc
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-31 04:12:51 +0000

    Disabled inject version until its implemented

--- inject-version.sh
+++
@@ -1,8 +0,0 @@
-VERSION=$(cat version.txt)
-BUILD_NUMBER=$(cat buildnumber.txt)
-
-FULL_VERSION="$VERSION-$BUILD_NUMBER"
-
-SOURCE_FILE="src/sketch/Serial1602ShieldSystemUI/Serial1602ShieldSystemUI.ino"
-
-sed -i "s/#define VERSION .*/#define VERSION \"$FULL_VERSION\"/" $SOURCE_FILE
